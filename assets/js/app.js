93538ad5e16928a2ee55f8d2eee76cc5c599757a
Clemens Denzinger <clemi@firegore.com>
2020-02-23 15:23:43 +0100

    add support for WebpackEncore for Frontend
    including Bootstrap

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Shared JS File
+
+//require('../css/app.css');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+
+//Import Bootstrap4
+require('bootstrap');
+require('bootstrap/scss/bootstrap.scss');
