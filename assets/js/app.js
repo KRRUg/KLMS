80ca08e7d7f704affada2af2d3d0fdd9db1d5fd2
Coffeemaker <coffeemaker@krru.at>
2020-02-25 00:38:32 +0100

    Adderd base site structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,8 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 //Import Bootstrap4
 require('bootstrap');
 require('../css/_scss/bootstrap-krru.scss');
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+
+//Images
+require('../images/test_content_pic.jpg');
