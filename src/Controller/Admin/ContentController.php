e3f0903a56793a560504caa036fdb3b72e3d5bf4
Thomas Hader <thader@drei.at>
2020-03-19 18:37:29 +0100

    Showing of control admin page done, no edits yet

--- src/Controller/Admin/ContentController.php
+++ src/Controller/Admin/ContentController.php
@@ -3,11 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Content;
+use App\Entity\NavigationNodeContent;
+use App\Entity\NavigationNodeGeneric;
 use App\Form\ContentType;
 use App\Repository\ContentRepository;
 use App\Repository\NavigationRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,13 +21,30 @@ class ContentController extends AbstractController
     /**
      * @Route("/content", name="content")
      */
-    public function index(NavigationRepository $navigationRepository,
+    public function index(Request $request,
+                          NavigationRepository $navigationRepository,
                           ContentRepository $contentEntryRepository) {
         $main = $navigationRepository->getRootChildren();
-        $content = $contentEntryRepository->findAll();
+        $id = $request->get('id');
+
+        $current = null;
+        if ($id)
+            $current = $navigationRepository->find($id);
+
+        $form = null;
+        if ($current instanceof NavigationNodeContent) {
+            $form = $this->createForm(ContentType::class, $current->getContent())->createView();
+        } else if ($current instanceof NavigationNodeGeneric) {
+            $defaultData = ['path' => $current->getPath()];
+            $form = $this->createFormBuilder($defaultData)
+                ->add('path', TextType::class)
+                ->add('save', SubmitType::class, ['label' => 'Speichern'])
+                ->getForm()->createView();
+        }
+
         return $this->render("admin/content/index.html.twig", [
             'tree' => $main,
-            'content' => $content
+            'form' => $form
         ]);
     }
 
--- src/Form/ContentType.php
+++ src/Form/ContentType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ContentType extends AbstractType
 {
