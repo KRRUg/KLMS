872805aac5b62ef58d461f4bfab8114cc82f010a
Clemens <firegore@users.noreply.github.com>
2020-03-22 22:38:53 +0100

    Feature/user mgmt (#5)
    * User login controller done, waiting for IDM
    
    * Created Admin and Gamer registration check
    
    * add IDM Connection to UserProvider
    
    Co-authored-by: Thomas Hader <thader@drei.at>

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\UserAdmin;
+use App\Entity\UserGamer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $admin = new UserAdmin();
+        $admin->setId("c11ed9b0-e060–4aec-b513-e17c24df2c70");
+        $manager->persist($admin);
+
+        $gamer = new UserGamer();
+        $gamer->setId("a3ba1298-4aa0–4aa1-5bb2-e18c98fa0980");
+        $manager->persist($gamer);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/UserAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserAdminsRepository")
+ */
+class UserAdmin
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="guid", unique=true)
+     */
+    private $guid;
+
+    public function getId(): ?string
+    {
+        return $this->guid;
+    }
+
+    public function setId(string $guid)
+    {
+        $this->guid = $guid;
+    }
+}
