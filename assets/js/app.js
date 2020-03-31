f8413e3dcc69b60cb8b6ebcfc4587a8d92ef76e3
Philipp Schütz <coffeemaker@krru.at>
2020-04-01 00:31:41 +0200

    Feature/design frontend (#8)
    * Styles for startpage, news and content
    
    * Added Frontend Styles
    
    * div styles
    
    * Nav Menu fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 
 //Import Bootstrap4
+require('jquery');
 require('bootstrap');
 require('../css/_scss/bootstrap-krru.scss');
 
@@ -13,3 +14,5 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 
 //Images
 require('../images/test_content_pic.jpg');
+require('../images/header_bg.jpg');
+require('../images/krru-white.png');
