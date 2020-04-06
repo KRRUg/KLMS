18dc98178d2c2974b08359e7ac112a4393dccc8d
Thomas Hader <thader@drei.at>
2020-04-07 00:48:10 +0200

    Some fixes in GamerController.php

--- src/Controller/Admin/GamerController.php
+++ src/Controller/Admin/GamerController.php
@@ -4,28 +4,31 @@
 namespace App\Controller\Admin;
 
 
-use App\Repository\NewsRepository;
+use App\Exception\UserServiceException;
 use App\Service\GamerService;
 use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class GamerController extends AbstractController
 {
     private $gamerService;
+    private $userService;
 
     public function __construct(UserService $userService, GamerService $gamerService)
     {
+        $this->userService = $userService;
         $this->gamerService = $gamerService;
     }
 
     /**
      * @Route("/gamer", name="gamer")
      */
-    public function index(NewsRepository $newsEntryRepository)
+    public function index(Request $request)
     {
-        $gamers = $this->gamerService->getGamerWithStatus();
+        $gamers = $this->gamerService->getRegisteredGamer();
         return $this->render("admin/gamer/index.html.twig", [
             'gamers' => $gamers
         ]);
