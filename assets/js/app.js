dc3946b0034b46302d06d55c8af226ce97f4d845
Thomas Hader <thader@drei.at>
2020-05-31 21:01:41 +0200

    refactored tinymce init code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,11 @@
 // Shared JS File
 
-//require('../css/app.css');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-
-
 //Import Bootstrap4
 require('jquery');
 require('bootstrap');
 require('../css/_scss/bootstrap-krru.scss');
 
 require('@fortawesome/fontawesome-free/css/all.min.css');
-
-require('select2');
-require('select2/dist/css/select2.css');
-require('select2-bootstrap4-theme/dist/select2-bootstrap4.min.css');
-require('select2/dist/js/i18n/de.js');
-//Images
 
 let select2 = require('./modules/select2.js');
 
