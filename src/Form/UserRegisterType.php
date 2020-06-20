86a5e3af037076ce6ada9d12fcf3e2bca196d9d1
Clemens D <firegore@users.noreply.github.com>
2020-06-20 20:52:58 +0200

    allow setting infoMails on Registration

--- src/Form/UserRegisterType.php
+++ src/Form/UserRegisterType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -45,6 +46,9 @@ class UserRegisterType extends AbstractType
                 'constraints' => [
                     new NotBlank()
                 ]
+            ])
+            ->add('infoMails', CheckboxType::class, [
+                'label' => 'Newsletter abonnieren. Weitere Infos zum Newsletter findest du in unserer Datenschutzbestimmung <TODO: LINK EINFÜGEN>'
             ])
             ->add('save', SubmitType::class, ['label' => 'Registrieren'])
 
