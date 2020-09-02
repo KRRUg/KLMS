dc2a8ed7c14d30a162da69097dd93f32a04a8c36
Thomas Hader <thader@drei.at>
2020-09-02 22:03:11 +0200

    Showing date as one input

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\News;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -17,8 +17,16 @@ class NewsType extends AbstractType
         $builder
             ->add('title')
             ->add('content', TextareaType::class, ['required' => false, 'empty_data' => ''])
-            ->add('publishedFrom')
-            ->add('publishedTo')
+            ->add('publishedFrom', DateTimeType::class, [
+                'with_seconds' => false,
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('publishedTo', DateTimeType::class, [
+                'with_seconds' => false,
+                'widget' => 'single_text',
+                'required' => false,
+            ])
         ;
         $builder->add('imageFile', VichImageType::class, [
             'required' => false,
