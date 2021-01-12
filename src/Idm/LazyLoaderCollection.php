ddb8d0cfc9371e7d5c406e5c703a6eddf0cb70bb
Thomas Hader <thader@drei.at>
2021-01-12 18:39:45 +0100

    LazyLoaderCollection done

--- src/Idm/LazyLoaderCollection.php
+++ src/Idm/LazyLoaderCollection.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Idm;
 
 use ArrayAccess;
@@ -15,37 +14,55 @@ class LazyLoaderCollection implements ArrayAccess, Iterator
     private IdmManager $manager;
     private string $class;
 
-    // array <id, item>
+    // the items array with management information
     private array $items;
-    private array $ids;
+
+    private const I_ID = 0;
+    private const I_OBJ = 1;
+    private const I_ADD = 2;
+    private const I_REM = 3;
 
     public function __construct(IdmManager $manager, string $class, array $uuids = [])
     {
         $this->manager = $manager;
         $this->class = $class;
-        $this->ids = $uuids;
+
         $this->items = [];
+        foreach ($uuids as $uuid) {
+            $this->items[] = [self::I_ID => $uuid, self::I_OBJ => null, self::I_ADD => false, self::I_REM => false];
+        }
     }
 
     private function get($offset)
     {
-        if (!isset($this->ids[$offset]))
+        if (!isset($this->items[$offset]))
             return null;
-        $id = $this->ids[$offset];
-        if (isset($this->items[$id]))
-            return $this->items[$id];
-        return $this->items[$id] = $this->manager->request($this->class, $id);
+        $item =& $this->items[$offset];
+        if ($item[self::I_REM])
+            return null;
+        if (empty($item[self::I_OBJ]))
+            $item[self::I_OBJ] = $this->manager->request($this->class, $item[self::I_ID]);
+
+        return $item[self::I_OBJ];
     }
 
-    public function isLoaded($offset)
-    {
-        return isset($this->ids[$offset]) && isset($this->items[$this->ids[$offset]]);
-    }
+//    private function put($id, $object = null, bool $removed = false, bool $added = false)
+//    {
+//        $this->items[] = [self::I_ID => $id, self::I_OBJ => $object, self::I_ADD => $removed, self::I_REM => $added];
+//    }
 
     public function offsetExists($offset): bool
     {
-        return isset($this->ids[$offset]);
+        return isset($this->items[$offset]) && !$this->items[$offset][self::I_REM];
     }
+
+//    public function isLoaded($offset)
+//    {
+//        if (!isset($this->items[$offset]))
+//            return false;
+//        $item = $this->items[$offset];
+//        return !$item[self::I_REM] && !empty($item[self::I_OBJ]);
+//    }
 
     public function offsetGet($offset)
     {
@@ -57,42 +74,53 @@ class LazyLoaderCollection implements ArrayAccess, Iterator
         if (!is_a($value, $this->class)) {
             throw new InvalidArgumentException("Incorrect type");
         }
-        if ($this->isLoaded($offset))
-            unset($this->items[$this->ids[$offset]]);
-        $id = $value->getUuid();
-        $this->ids[] = $id;
-        $this->items[$id] = $value;
+        // move element to new position
+        if (isset($this->items[$offset])) {
+            $item = $this->items[$offset];
+            $item[self::I_REM] = true;
+            if (!$item[self::I_ADD])
+                $this->items[] = $item;
+        }
+        $this->items[$offset] = [self::I_ID => null, self::I_OBJ => $value, self::I_ADD => true, self::I_REM => false];
     }
 
     public function offsetUnset($offset)
     {
-        if ($this->isLoaded($offset))
-            unset($this->items[$this->ids[$offset]]);
-        unset($this->ids[$offset]);
+        if (!$this->offsetExists($offset))
+            return;
+        $this->items[$offset][self::I_REM] = true;
     }
 
     public function current()
     {
-        return $this->get(key($this->ids));
+        return $this->get(key($this->items));
     }
 
     public function next()
     {
-        next($this->ids);
+        while($val = next($this->items)) {
+            if (!$val[self::I_REM])
+                break;
+        }
     }
 
     public function key()
     {
-        return key($this->ids);
+        return key($this->items);
     }
 
     public function valid(): bool
     {
-        return null !== key($this->ids);
+        return null !== key($this->items);
     }
 
     public function rewind()
     {
-        reset($this->ids);
+        if (!($val = reset($this->items)))
+            return;
+        do {
+            if (!$val[self::I_REM])
+                break;
+        } while($val = next($this->items));
     }
 }
