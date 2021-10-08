e1e0a120aa06855094dab727009c9b96cbf5a5b7
Thomas Hader <thader@drei.at>
2021-10-08 17:06:15 +0200

    Fixed isSeatOwner and added Seatmap to ContentFixture

--- src/DataFixtures/ContentFixture.php
+++ src/DataFixtures/ContentFixture.php
@@ -167,7 +167,7 @@ class ContentFixture extends Fixture
         $nav = new Navigation();
         $nav->setName('main_menu');
         $nav->setMaxDepth(2);
-        $nav->addNode((new NavigationNodeRoot())->setPos(1,18));
+        $nav->addNode((new NavigationNodeRoot())->setPos(1,20));
         $nav->addNode((new NavigationNodeGeneric())->setName("Home")->setPos(2,3));
         $nav->addNode((new NavigationNodeEmpty())->setName("Lan Party")->setPos(4, 15));
         $nav->addNode((new NavigationNodeContent($content[0]))->setName("Facts")->setPos(5, 6));
@@ -176,6 +176,7 @@ class ContentFixture extends Fixture
         $nav->addNode((new NavigationNodeContent($content[1]))->setName("FAQ")->setPos(11,12));
         $nav->addNode((new NavigationNodeContent($content[2]))->setName("Location")->setPos(13, 14));
         $nav->addNode((new NavigationNodeTeamsite($ts[0]))->setName("Team")->setPos(16,17));
+        $nav->addNode((new NavigationNodeGeneric('/seatmap'))->setName("Sitzplan")->setPos(18,19));
         $manager->persist($nav);
 
         $footer = new Navigation();
