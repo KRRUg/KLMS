fe09febe1bcf5abfaba156640f4c4af07bd65412
Thomas Hader <thader@drei.at>
2022-09-07 18:34:44 +0200

    Added Comment entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="uuid")
+     */
+    private $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=News::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $news;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getNews(): ?News
+    {
+        return $this->news;
+    }
+
+    public function setNews(?News $news): self
+    {
+        $this->news = $news;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+}
--- src/Entity/News.php
+++ src/Entity/News.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Helper\HistoryAwareEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -60,11 +62,17 @@ class News implements HistoryAwareEntity
      */
     private $image;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="news", orphanRemoval=true)
+     */
+    private $comments;
+
     use Traits\EntityHistoryTrait;
 
     public function __construct()
     {
         $this->image = new EmbeddedFile();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -160,5 +168,35 @@ class News implements HistoryAwareEntity
         } else {
             return $this->getPublishedFrom();
         }
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setNews($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getNews() === $this) {
+                $comment->setNews(null);
+            }
+        }
+
+        return $this;
     }
 }
