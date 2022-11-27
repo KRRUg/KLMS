e10143172699405a42f266144cb3c6919f8cec08
Ovascos <Ovascos@users.noreply.github.com>
2022-11-27 21:27:57 +0100

    Fixed sentry init when sentry is not defined (#89)

--- assets/js/modules/sentry/sentry.js
+++ assets/js/modules/sentry/sentry.js
@@ -2,8 +2,12 @@ import * as Sentry from '@sentry/browser/esm';
 
 if (process.env.NODE_ENV === 'production') {
     // Code will only appear in production build.
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN,
-        release: process.env.APP_VERSION
-    });
+    try {
+        Sentry.init({
+            dsn: process.env.SENTRY_DSN,
+            release: process.env.APP_VERSION
+        });
+    } catch (e) {
+        // sentry init error
+    }
 }
