2927715b8f14b583b950320021671e732bc3d4ed
Thomas Hader <thader@drei.at>
2022-11-27 17:56:43 +0100

    Initialize Sentry in production only

--- assets/js/modules/sentry/sentry.js
+++ assets/js/modules/sentry/sentry.js
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/browser/esm';
 
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    release: process.env.APP_VERSION
-});
+if (process.env.NODE_ENV === 'production') {
+    // Code will only appear in production build.
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        release: process.env.APP_VERSION
+    });
+}
