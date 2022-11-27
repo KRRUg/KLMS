faab00aafea541a7296eb71d66b7f1f986c366b0
Thomas Hader <thader@drei.at>
2022-11-27 17:11:50 +0100

    recipes:update symfony/webpack-encore

--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,11 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
+    // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
@@ -35,6 +32,9 @@ Encore
     .addEntry('site_seatmap', './assets/js/site/seatmap.js')
     .addEntry('admin_seatmap', './assets/js/admin/seatmap.js')
     .addStyleEntry('email', './assets/css/email.scss')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    //.enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -66,10 +66,15 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-    // enables @babel/preset-env polyfills
+    // configure Babel
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
+
+    // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = 3;
+        config.corejs = '3.23';
     })
 
     // enables Sass/SCSS support
@@ -79,16 +84,15 @@ Encore
     //.enableTypeScriptLoader()
     //.enableBabelTypeScriptPreset()
 
+    // uncomment if you use React
+    //.enableReactPreset()
+
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
     .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
-
-    // uncomment if you use API Platform Admin (composer require api-admin)
-    //.enableReactPreset()
-    //.addEntry('admin', './assets/js/admin.js')
 
     // enable WebPack5 Build Caching (EXPERIMENTAL)
     //.enableBuildCache()
