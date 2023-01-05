b51e2f2dc13ccf8668bc6dbf5feade8990803fe0
Thomas Hader <thader@drei.at>
2023-01-05 22:33:31 +0100

    Moved to Controller

--- src/Controller/Site/NewsController.php
+++ src/Controller/Site/NewsController.php
@@ -5,12 +5,16 @@ namespace App\Controller\Site;
 use App\Entity\News;
 use App\Service\NewsService;
 use App\Service\UserService;
+use DateTime;
 use Eko\FeedBundle\Feed\FeedManager;
 use Eko\FeedBundle\Field\Item\MediaItemField;
+use Eko\FeedBundle\Item\Writer\ItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 #[Route(path: '/news', name: 'news')]
 class NewsController extends AbstractController
@@ -18,17 +22,21 @@ class NewsController extends AbstractController
     private readonly NewsService $newsService;
     private readonly UserService $userService;
     private readonly FeedManager $feedManager;
+    private readonly RouterInterface $router;
+    private readonly UploaderHelper $vich;
 
     private const PRELOAD_NEWS_CNT = 6;
 
     /**
      * NewsController constructor.
      */
-    public function __construct(NewsService $newsService, UserService $userService, FeedManager $feedManager)
+    public function __construct(NewsService $newsService, UserService $userService, FeedManager $feedManager, RouterInterface $router, UploaderHelper $vich)
     {
         $this->newsService = $newsService;
         $this->userService = $userService;
         $this->feedManager = $feedManager;
+        $this->router = $router;
+        $this->vich = $vich;
     }
 
     #[Route(path: '', name: '')]
@@ -72,13 +80,29 @@ class NewsController extends AbstractController
     }
 
     #[Route(path: '/feed.rss', name: '_feed')]
-    public function feed(): Response
+    public function feed(Request $request): Response
     {
         $news = $this->newsService->get();
-        $news = array_map($this->newsService->toFeedElement(...), $news);
+        $news = array_map(fn($n) => $this->toFeedElement($request->getSchemeAndHttpHost(), $n), $news);
         $feed = $this->feedManager->get('news');
         $feed->addFromArray($news);
         $feed->addItemField(new MediaItemField('getFeedItemImage'));
         return new Response($feed->render('rss'), 200, ['Content-Type' => 'application/rss+xml']);
+    }
+
+    private function toFeedElement(string $host, News $news): ItemInterface
+    {
+        $url = $host . $this->router->generate('news_detail', ['id' => $news->getId()], RouterInterface::ABSOLUTE_PATH);
+        if (null !== $img_url = $this->vich->asset($news)){
+            $img_url = $host . $img_url;
+        }
+        return new class ($news, $url,  $img_url) implements ItemInterface {
+            public function __construct(private readonly News $news, private readonly string $url, private readonly ?string $img_url){}
+            public function getFeedItemTitle(): string { return $this->news->getTitle() ?? ""; }
+            public function getFeedItemDescription(): string { return $this->news->getContent() ?? ""; }
+            public function getFeedItemPubDate(): DateTime { return DateTime::createFromInterface($this->news->activeSince()); }
+            public function getFeedItemLink(): string { return $this->url; }
+            public function getFeedItemImage(): array { return empty($this->img_url) ? [] : ['type' => $this->news->getImage()->getMimeType(), 'length' => $this->news->getImage()->getSize(), 'value' => $this->img_url]; }
+        };
     }
 }
--- src/Service/NewsService.php
+++ src/Service/NewsService.php
@@ -4,29 +4,23 @@ namespace App\Service;
 
 use App\Entity\News;
 use App\Repository\NewsRepository;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Eko\FeedBundle\Item\Writer\RoutedItemInterface;
 use Psr\Log\LoggerInterface;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelperInterface;
 
 class NewsService
 {
     private readonly NewsRepository $repo;
     private readonly EntityManagerInterface $em;
     private readonly LoggerInterface $logger;
-    private readonly UploaderHelper $uli;
 
     /**
      * NewsService constructor.
      */
-    public function __construct(NewsRepository $repo, EntityManagerInterface $em, LoggerInterface $logger, UploaderHelper $uli)
+    public function __construct(NewsRepository $repo, EntityManagerInterface $em, LoggerInterface $logger)
     {
         $this->repo = $repo;
         $this->em = $em;
         $this->logger = $logger;
-        $this->uli = $uli;
     }
 
     /**
@@ -64,20 +58,5 @@ class NewsService
         $this->logger->info("Create or Update News {$news->getId()} ({$news->getTitle()})");
         $this->em->persist($news);
         $this->em->flush();
-    }
-
-    public function toFeedElement(News $news): RoutedItemInterface
-    {
-        $img_url = $this->uli->asset($news);
-        return new class ($news, $img_url) implements RoutedItemInterface {
-            public function __construct(private readonly News $news, private readonly ?string $img_url){}
-            public function getFeedItemTitle(): string { return $this->news->getTitle() ?? ""; }
-            public function getFeedItemDescription(): string { return $this->news->getContent() ?? ""; }
-            public function getFeedItemRouteName(): string { return 'news_detail'; }
-            public function getFeedItemRouteParameters(): array { return ['id' => $this->news->getId()]; }
-            public function getFeedItemUrlAnchor(): string { return ''; }
-            public function getFeedItemPubDate(): DateTime { return DateTime::createFromInterface($this->news->activeSince()); }
-            public function getFeedItemImage(): array { return empty($this->img_url) ? [] : ['type' => $this->news->getImage()->getMimeType(), 'length' => $this->news->getImage()->getSize(), 'value' => $this->img_url]; }
-        };
     }
 }
