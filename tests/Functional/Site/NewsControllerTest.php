8374822d2ac60e1a75f5644f350ec9c31b8f8771
Thomas Hader <thader@drei.at>
2023-05-28 00:42:59 +0200

    Added further tests

--- tests/Functional/Site/NewsControllerTest.php
+++ tests/Functional/Site/NewsControllerTest.php
@@ -4,19 +4,14 @@ namespace App\Tests\Functional\Site;
 
 use App\Tests\Functional\DatabaseWebTestCase;
 
-class UserControllerTest extends DatabaseWebTestCase
+class NewsControllerTest extends DatabaseWebTestCase
 {
-    public function testHomepageLoad()
-    {
-        $this->databaseTool->loadAllFixtures();
-        $crawler = $this->client->request('GET', '/');
-        $this->assertResponseStatusCodeSame(200);
-    }
-
     public function testNewsLoad()
     {
         $this->databaseTool->loadAllFixtures();
         $crawler = $this->client->request('GET', '/');
         $this->assertResponseStatusCodeSame(200);
+        $div_news = $crawler->filter('#news');
+        $this->assertEquals(6, $div_news->children()->count());
     }
 }
