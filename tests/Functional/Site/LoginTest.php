0358430272e27b3127e82a3c2cfeaac47007c60f
Thomas Hader <thader@drei.at>
2023-06-17 20:29:38 +0200

    Logout test done

--- tests/Functional/Site/LoginTest.php
+++ tests/Functional/Site/LoginTest.php
@@ -9,7 +9,6 @@ class LoginTest extends DatabaseWebTestCase
     public function testLoginAndLogout()
     {
         $this->databaseTool->loadFixtures([]);
-
         $this->client->followRedirects();
 
         $crawler = $this->client->request('GET', '/');
@@ -23,9 +22,30 @@ class LoginTest extends DatabaseWebTestCase
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertSelectorTextContains('#dropdownMenuUser', "User 2");
+        $this->assertSelectorNotExists('.alert-error');
 
         $this->client->request('GET', '/logout');
         $this->assertResponseStatusCodeSame(200);
         $this->assertSelectorTextContains('#dropdownMenuUser', "Anmelden");
+    }
+
+    public function testLoginUserNotFound()
+    {
+        $this->databaseTool->loadFixtures([]);
+
+        $crawler = $this->client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertSelectorTextContains('#dropdownMenuUser', "Anmelden");
+
+        $form = $crawler->selectButton('Login')->form();
+        $form['username']->setValue('unkown@localhost.local');
+        $form['password']->setValue('pa$$word');
+        $this->client->submit($form);
+
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertResponseRedirects('/login');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('#dropdownMenuUser', "Anmelden");
+        $this->assertSelectorExists('.alert-error');
     }
 }
