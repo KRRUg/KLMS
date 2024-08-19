da571e83668400f9c64ae13aa7120acb19ce967f
Coffeemaker <coffeemaker@krru.at>
2024-08-19 21:06:14 +0200

    Added Twig Filter to get the average color of an Image. Used in Email as bg-color

---
+++ src/Twig/AverageColorFromImageFilterExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Description of AgoFilterExtension.
+ */
+class AverageColorFromImageFilterExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('average_color', $this->getAverageColorFromImage(...)),
+        ];
+    }
+
+    public function getAverageColorFromImage($imagePath): string
+    {
+
+        $image = $this->createImageFromFile($imagePath);
+
+        if($image === false) {
+            return '#000000';
+        }
+
+        $pixIm = imagescale($image, 1,1);
+        $rgb = imagecolorsforindex($pixIm, imagecolorat($pixIm, 0, 0));
+
+        return sprintf("#%02x%02x%02x", $rgb["red"], $rgb["green"], $rgb["blue"]);
+    }
+
+    private function createImageFromFile(string $imagePath): \GdImage|false {
+        $info = getimagesize($imagePath);
+        $mime = image_type_to_mime_type($info[2]);
+        return match ($mime) {
+            'image/jpeg' => imagecreatefromjpeg($imagePath),
+            'image/png' => imagecreatefrompng($imagePath),
+            'image/gif' => imagecreatefromgif($imagePath),
+            default => false,
+        };
+
+    }
+}
