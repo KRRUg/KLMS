c1cf56f17a419ed8fe1426d48a5cbe7d998a4010
Firegore <firegore@users.noreply.github.com>
2025-03-15 01:08:53 +0100

    initial WIP

---
+++ src/Controller/Admin/CheckinController.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Ticket;
+use App\Entity\User;
+use App\Entity\UserImage;
+use App\Exception\TicketLivecycleException;
+use App\Form\UserSelectType;
+use App\Form\UserType;
+use App\Repository\UserImageRepository;
+use App\Service\TicketService;
+use App\Service\TicketState;
+use App\Service\UserService;
+use Doctrine\DBAL\Types\StringType;
+use Ramsey\Uuid\Doctrine\UuidType;
+use Ramsey\Uuid\UuidInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[IsGranted('ROLE_ADMIN_PAYMENT')]
+#[Route(path: '/checkin', name: 'checkin')]
+class CheckinController extends AbstractController
+{
+    private readonly TicketService $ticketService;
+    private readonly UserService $userService;
+    private readonly UserImageRepository $userImgRepo;
+
+    public function __construct(TicketService $ticketService,
+                                UserService   $userService,
+                                UserImageRepository $userImgRepo)
+    {
+        $this->ticketService = $ticketService;
+        $this->userService = $userService;
+        $this->userImgRepo = $userImgRepo;
+    }
+
+    private function createUserSelectForm(): FormInterface
+    {
+        $form = $this->createFormBuilder();
+        $form->add('user', UserSelectType::class);
+
+        return $form->getForm();
+    }
+
+    private function createTicketModificationForm(Ticket $ticket): FormInterface
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_checkin_update', ['id' => $ticket->getId()]));
+        $can_delete_ticket = empty($ticket->getShopOrderPosition());
+        switch ($ticket->getState()) {
+            case TicketState::REDEEMED:
+                $form->add('punch', SubmitType::class);
+                break;
+            case TicketState::PUNCHED:
+                $form->add('unpunch', SubmitType::class);
+                break;
+        }
+        return $form->getForm();
+    }
+
+    #[Route(path: '', name: '', methods: ['GET'])]
+    public function index(): Response
+    {
+        $tickets = $this->ticketService->queryTickets();
+        $uuids = array_map(fn (Ticket $t) => $t->getRedeemer(), $tickets);
+        $uuids = array_filter($uuids, fn (?UuidInterface $uuid) => !empty($uuid));
+        $users = $this->userService->getUsers($uuids, assoc: true);
+
+        return $this->render('admin/checkin/index.html.twig', [
+            'tickets' => $tickets,
+            'users' => $users,
+            'form_add' => $this->createUserSelectForm()->createView(),
+        ]);
+    }
+
+    // TODO add create new Ticket Controllerd
+
+    #[Route(path: '', name: '_add', methods: ['POST'])]
+    public function add(Request $request): Response
+    {
+        $form = $this->createUserSelectForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData()['user'];
+            if (empty($user)) {
+                $this->addFlash('error', 'Ungültigen User ausgewählt.');
+            } elseif ($this->ticketService->isUserRegistered($user)) {
+                $this->addFlash('warning', "User {$user->getNickname()} ist schon registriert.");
+            } else {
+                try {
+                    $ticket = $this->ticketService->registerUser($user);
+                    $this->addFlash('success', "User {$user->getNickname()} wurde zur Veranstaltung mit Ticket {$ticket->getCode()} registriert.");
+                } catch (TicketLivecycleException) {
+                    $this->addFlash('error', "User {$user->getNickname()}  konnte nicht registriert werden.");
+                }
+            }
+        }
+
+        return $this->redirectToRoute('admin_checkin');
+    }
+
+    private static function clickedIfExists(FormInterface $form, string $field): bool
+    {
+        return $form->has($field) ? $form->get($field)->isClicked() : false;
+    }
+
+    #[Route(path: '/{id}', name: '_update', methods: ['POST'])]
+    public function update(Request $request, Ticket $ticket): Response
+    {
+        $form = $this->createTicketModificationForm($ticket);
+        $form->handleRequest($request);
+        $id = $ticket->getId();
+        $error = "";
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                switch (true) {
+                    case self::clickedIfExists($form, 'assign'):
+                        $user = $form->get('user')->getData();
+                        if ($this->ticketService->isUserRegistered($user)) {
+                            $error = "User {$user->getNickname()} ist schon registriert.";
+                        } else {
+                            $this->ticketService->redeemTicket($ticket, $user);
+                        }
+                        break;
+                    case self::clickedIfExists($form, 'unassign'):
+                        $this->ticketService->unassignTicket($ticket);
+                        break;
+                    case self::clickedIfExists($form, 'punch'):
+                        $this->ticketService->punchTicket($ticket);
+                        break;
+                    case self::clickedIfExists($form, 'unpunch'):
+                        $this->ticketService->unpunchTicket($ticket);
+                        break;
+                    case self::clickedIfExists($form, 'delete'):
+                        $this->ticketService->deleteTicket($ticket);
+                        break;
+                    default:
+                        $this->addFlash('error', "Aktion konnte nicht durchgeführt werden");
+                        return $this->redirectToRoute('admin_checkin');
+                }
+            } catch (TicketLivecycleException $exception) {
+                $this->addFlash('error', "Aktion konnte nicht durchgeführt werden ({$exception->getMessage()}).");
+                return $this->redirectToRoute('admin_checkin');
+            }
+            if (!empty($error)) {
+                $this->addFlash('error', $error);
+            } else {
+                $this->addFlash('success', "Änderung an Ticket #{$id} erfolgreich.");
+            }
+        }
+
+        return $this->redirectToRoute('admin_checkin');
+    }
+
+    #[Route(path: '/{id}', name: '_show', methods: ['GET'])]
+    public function show(Request $request, Ticket $ticket): Response
+    {
+        $form = $this->createTicketModificationForm($ticket);
+        $user = $this->ticketService->userByTicket($ticket);
+        $image = $this->userImgRepo->findOneByUser($user) ?? new UserImage($user->getUuid());
+        $form2 = $this->createForm(UserType::class, $user, ['disable_on_lock' => false, 'with_image' => true]);
+        $form2->get('image')->setData($image);
+
+        $form3 = $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_checkin_update', ['id' => $ticket->getId()]));
+        $form3->add('klcsUuid', TextType::class);
+
+        return $this->render('admin/checkin/show.html.twig', [
+            'user' => $user,
+            'ticket' => $ticket,
+            'form' => $form->createView(),
+            'form2' => $form2->createView(),
+            'form3' => $form3->getForm()->createView()
+        ]);
+    }
+}
